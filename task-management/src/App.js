53b9c2c42901e622da0ec01ff52f0e129d61b6e2
Santhi Gummidi <95281822+santhi-225@users.noreply.github.com>
2022-11-26 16:08:05 +0530

    refactor: route paths updated as constants

--- task-management/src/App.js
+++ task-management/src/App.js
@@ -3,16 +3,17 @@ import Home from "./components/Home";
 import Login from "./components/Login";
 import Reports from "./components/Reports";
 import './App.css'
-
+import { places } from "./constants/constants";
+const { REPORTS, LOGIN, HOME} = places
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/reports" element={<Reports />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/login" element={<Login />} />
+        <Route path={REPORTS} element={<Reports />} />
+        <Route path={HOME} element={<Home />} />
+        <Route path={LOGIN} element={<Login />} />
       </Routes>
       </BrowserRouter>
     </div>
