5af72960041076ca6299c992fb694e4add48873c
Santhi Gummidi <95281822+santhi-225@users.noreply.github.com>
2022-11-26 20:09:49 +0530

    Layout Added with Header and Task list and add task
    Layout Added with Header and Task list and add task

--- task-management/src/components/Home.js
+++ task-management/src/components/Home.js
@@ -1,7 +1,22 @@
+import { Layout } from "antd";
+import { Content, Header } from "antd/es/layout/layout";
 import React from "react";
+import AddTask from "./AddTask";
+import AppHeader from "./AppHeader";
+import TaskList from "./TaskList";
 const Home = () => {
-    return(
-        <div>Home page</div>
-    )
-}
-export default Home
+  return (
+    <Layout className="site-layout">
+      <Header>
+        <AppHeader />
+      </Header>
+      <Layout>
+        <Content>
+          <TaskList />
+          <AddTask />
+        </Content>
+      </Layout>
+    </Layout>
+  );
+};
+export default Home;
