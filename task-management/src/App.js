0e7d7496283e07ae36238114442352c931b950e1
Santhi Gummidi <95281822+santhi-225@users.noreply.github.com>
2022-11-26 16:43:43 +0530

    authentication added for home and report routes
    redirecting user to login page, if trying to vising home or reports without logging in

--- task-management/src/App.js
+++ task-management/src/App.js
@@ -1,18 +1,24 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./components/Home";
 import Login from "./components/Login";
 import Reports from "./components/Reports";
 import './App.css'
 import { places } from "./constants/constants";
+import useAuth from "./hooks/useAuth";
 const { REPORTS, LOGIN, HOME} = places
+
+const RequireAuth =({ children }) =>{
+  const { isLoggedIn } = useAuth();
+  return isLoggedIn ? children : <Navigate to={LOGIN} replace />;
+}
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path={REPORTS} element={<Reports />} />
-        <Route path={HOME} element={<Home />} />
+        <Route path={REPORTS} element={<RequireAuth><Reports /></RequireAuth>} />
+        <Route path={HOME} element={ <RequireAuth><Home /></RequireAuth>} />
         <Route path={LOGIN} element={<Login />} />
       </Routes>
       </BrowserRouter>
