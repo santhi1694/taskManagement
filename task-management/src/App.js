82f73f1c6eceb48f4a061274827d15dea5923ccb
Santhi Gummidi <95281822+santhi-225@users.noreply.github.com>
2022-11-26 16:50:50 +0530

    fix: redirecting user to previous path post login
    redirecting to previous path post login if its there or else home route
    
    fixed formatting issues

--- task-management/src/App.js
+++ task-management/src/App.js
@@ -1,26 +1,52 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate,
+  useLocation,
+} from "react-router-dom";
 import Home from "./components/Home";
 import Login from "./components/Login";
 import Reports from "./components/Reports";
-import './App.css'
+import "./App.css";
 import { places } from "./constants/constants";
 import useAuth from "./hooks/useAuth";
-const { REPORTS, LOGIN, HOME} = places
+const { REPORTS, LOGIN, HOME } = places;
 
-const RequireAuth =({ children }) =>{
+const RequireAuth = ({ children }) => {
   const { isLoggedIn } = useAuth();
-  return isLoggedIn ? children : <Navigate to={LOGIN} replace />;
-}
+  const location = useLocation();
+
+  return isLoggedIn ? (
+    children
+  ) : (
+    <Navigate to={LOGIN} replace state={{ path: location.pathname }} />
+  );
+};
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path={REPORTS} element={<RequireAuth><Reports /></RequireAuth>} />
-        <Route path={HOME} element={ <RequireAuth><Home /></RequireAuth>} />
-        <Route path={LOGIN} element={<Login />} />
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route
+            path={REPORTS}
+            element={
+              <RequireAuth>
+                <Reports />
+              </RequireAuth>
+            }
+          />
+          <Route
+            path={HOME}
+            element={
+              <RequireAuth>
+                <Home />
+              </RequireAuth>
+            }
+          />
+          <Route path={LOGIN} element={<Login />} />
+        </Routes>
       </BrowserRouter>
     </div>
   );
--- task-management/src/hooks/useAuth.js
+++ task-management/src/hooks/useAuth.js
@@ -1,38 +1,37 @@
 import { message } from "antd";
 import { loginUser } from "../backend/api";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { useLocalStorage } from "./useLocalStorage";
 import { places } from "../constants/constants";
 
-const {HOME, LOGIN} = places
-
+const { HOME, LOGIN } = places;
 
 const useAuth = () => {
-    const [user, setUser] = useLocalStorage("user");
-    const navigate = useNavigate();
-  
-    const login = async (data) => {
-      const response = await loginUser(data);
-      const userdata = response.data.data;
-      message[response.type](response.data.message,5)
-      if (userdata) {
-          setUser(userdata)
-          navigate(HOME)
-      }
-      return response;
-    };
-  
-    const logout = () => {
-      setUser(null);
-      navigate(LOGIN, { replace: true });
-    };
-  
-    return {
-        isLoggedIn: !!user,
-        user,
-        login,
-        logout
+  const [user, setUser] = useLocalStorage("user");
+  const navigate = useNavigate();
+  const { state } = useLocation();
+
+  const login = async (data) => {
+    const response = await loginUser(data);
+    const userdata = response.data.data;
+    message[response.type](response.data.message, 5);
+    if (userdata) {
+      setUser(userdata);
+      navigate(state?.path || HOME);
     }
-  
+    return response;
+  };
+
+  const logout = () => {
+    setUser(null);
+    navigate(LOGIN, { replace: true });
+  };
+
+  return {
+    isLoggedIn: !!user,
+    user,
+    login,
+    logout,
+  };
 };
-export default useAuth
+export default useAuth;
