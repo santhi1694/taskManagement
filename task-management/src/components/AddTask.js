d6d3022b78c7f7b238de671cb71e6c36ee9b019a
Santhi Gummidi <95281822+santhi-225@users.noreply.github.com>
2022-11-26 20:36:27 +0530

    feat: Add Task Form added
    Ui for add task added with form fields title and duedate

--- task-management/src/components/AddTask.js
+++ task-management/src/components/AddTask.js
@@ -1,7 +1,45 @@
+import { Button, DatePicker, Form, Input } from "antd";
 import React from "react";
 const AddTask = () => {
+  const [form] = Form.useForm();
+  const onFinish = (values) => {
+    console.log("Success:", values);
+  };
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
   return (
-    <div className="add-task-container site-layout-background">Add task</div>
+    <div className="add-task-container site-layout-background">
+      <Form
+        name="addTask"
+        form={form}
+        layout="inline"
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
+      >
+        <Form.Item
+          label="Title"
+          name="title"
+          rules={[
+            {
+              required: true,
+              message: "Please add Title of your task!",
+            },
+          ]}
+        >
+          <Input.TextArea rows={1} />
+        </Form.Item>
+        <Form.Item label="Due Date" name="dueDate">
+          <DatePicker />
+        </Form.Item>
+        <Form.Item>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
   );
 };
 export default AddTask;
