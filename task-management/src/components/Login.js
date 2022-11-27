4664d3f0bca6e353bd2f624c162cd4034ba243b3
Santhi Gummidi <95281822+santhi-225@users.noreply.github.com>
2022-11-27 16:01:04 +0530

    fix: login page styling issues fixed

--- task-management/src/components/Login.js
+++ task-management/src/components/Login.js
@@ -1,75 +1,87 @@
-import React from 'react';
-import { Button,  Form, Input, message } from 'antd';
-import useAuth from '../hooks/useAuth';
-import { ERROR } from '../constants/constants';
+import React from "react";
+import { Button, Form, Input, message } from "antd";
+import useAuth from "../hooks/useAuth";
+import { ERROR } from "../constants/constants";
+import { UserOutlined } from "@ant-design/icons";
 
 const Login = () => {
-  const { login, } = useAuth();
+  const { login } = useAuth();
   const [form] = Form.useForm();
+
   const onFinish = (values) => {
-    login(values).then(response => {
-      if(response.type === ERROR) {
-        form.resetFields(['password'])
-      }
-    }).catch(() => {
-      message.error('Something went wrong!')
-    })
-    console.log('Success:', values);
+    login(values)
+      .then((response) => {
+        if (response.type === ERROR) {
+          form.resetFields(["password"]);
+        }
+      })
+      .catch(() => {
+        message.error("Something went wrong!");
+      });
+    console.log("Success:", values);
   };
+
   const onFinishFailed = (errorInfo) => {
-    console.log('Failed:', errorInfo);
+    console.log("Failed:", errorInfo);
+    message.error("Something went wrong!");
   };
   return (
-    <div className='login-container'>
-    <Form
-      name="basic"
-      form={form}
-      labelCol={{
-        span: 8,
-      }}
-      wrapperCol={{
-        span: 6,
-      }}
-      className="login-form"
-      onFinish={onFinish}
-      onFinishFailed={onFinishFailed}
-      autoComplete="off"
-    >
-      <Form.Item
-        label="Username"
-        name="username"
-        rules={[
-          {
-            required: true,
-            message: 'Please input your username!',
-          },
-        ]}
-      >
-        <Input />
-      </Form.Item>
-      <Form.Item
-        label="Password"
-        name="password"
-        rules={[
-          {
-            required: true,
-            message: 'Please input your password!',
-          },
-        ]}
-      >
-        <Input.Password />
-      </Form.Item>
-      <Form.Item
-        wrapperCol={{
-          offset: 8,
-          span: 16,
-        }}
-      >
-        <Button type="primary" htmlType="submit">
-          Submit
-        </Button>
-      </Form.Item>
-    </Form>
+    <div className="login-container">
+      <div className="login-form-container">
+        <div className="login-form">
+          <h1 className="login-header">
+            <UserOutlined />
+            Login
+          </h1>
+          <Form
+            name="basic"
+            form={form}
+            className="mt-3"
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            autoComplete="off"
+          >
+            <Form.Item
+              label="Username"
+              name="username"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your username!",
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item
+              label="Password"
+              name="password"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your password!",
+                },
+              ]}
+            >
+              <Input.Password />
+            </Form.Item>
+            <Form.Item
+              wrapperCol={{
+                offset: 8,
+              }}
+            >
+              <Button
+                type="primary"
+                htmlType="submit"
+                size="large"
+                className="login-btn"
+              >
+                Submit
+              </Button>
+            </Form.Item>
+          </Form>
+        </div>
+      </div>
     </div>
   );
 };
