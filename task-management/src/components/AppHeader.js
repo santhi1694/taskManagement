d9faeba305af1025e20c21acfc61d9d5687af90c
Santhi Gummidi <95281822+santhi-225@users.noreply.github.com>
2022-11-27 14:56:49 +0530

    feat: header added with hamburger
    feat: header added with hamburger

--- task-management/src/components/AppHeader.js
+++ task-management/src/components/AppHeader.js
@@ -1,7 +1,43 @@
+import { MenuOutlined } from "@ant-design/icons";
+import { Dropdown, Space } from "antd";
 import React from "react";
+import { NavLink } from "react-router-dom";
+import { places } from "../constants/constants";
+import useAuth from "../hooks/useAuth";
+const { HOME, REPORTS } = places;
+const items = [
+  {
+    key: "home",
+    label: <NavLink to={HOME}>Home Page</NavLink>,
+  },
+  {
+    key: "reports",
+    label: <NavLink to={REPORTS}>Business Reports</NavLink>,
+  },
+];
 const AppHeader = () => {
-    return(
-        <div>app header</div>
-    )
-}
+  const { logout } = useAuth();
+
+  const onLogout = (e) => {
+    e.preventDefault();
+    logout();
+  };
+  return (
+    <div className="app-header">
+      <Space>
+        <Dropdown
+          menu={{
+            items: [
+              ...items,
+              { key: "logout", label: <div onClick={onLogout}>Log out</div> },
+            ],
+          }}
+        >
+          <MenuOutlined className="menu-icon" />
+        </Dropdown>
+        <h1> Task Management </h1>
+      </Space>
+    </div>
+  );
+};
 export default AppHeader;
