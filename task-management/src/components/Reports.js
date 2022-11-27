2eae790d6454feb547a60332c893f894fba4abc8
Santhi Gummidi <95281822+santhi-225@users.noreply.github.com>
2022-11-27 08:56:16 +0530

    feat: reports layout added with table
    reports layout added with table of columns

--- task-management/src/components/Reports.js
+++ task-management/src/components/Reports.js
@@ -1,8 +1,45 @@
+import { Layout, Table, Tag } from "antd";
+import { Content } from "antd/es/layout/layout";
 import React from "react";
+import { covertTime } from "../utils/utils";
+const columns = [
+    {
+      title: "Titile",
+      dataIndex: "title",
+      key: "title",
+    },
+    {
+      title: "Due Date",
+      dataIndex: "dueDate",
+      key: "dueDate",
+      render: (value) => {
+        return <div>{covertTime(value)}</div>;
+      },
+    },
+    {
+      title: "Status",
+      dataIndex: "id",
+      key: "id",
+      render: (value) => {
+        const color = value ? 'green': 'amber'
+        return (
+          <div>
+            <Tag color={color}>
+            {value ? 'Completed' : 'Pending'}
+            </Tag>
+          </div>
+        );
+      },
+    },
+  ];
 
 const Reports = () => {
     return (
-        <div>Reports section</div>
+        <Layout className="site-layout">
+        <Content className="site-layout-background reports-container">
+          <Table dataSource={[]} columns={columns}/>          
+        </Content>
+      </Layout>
     )
 }
 export default Reports
